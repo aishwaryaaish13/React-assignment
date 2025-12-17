1e24c8bbc1905cbbcd67b760d8e719627c7adda5
aishwaryaaish13 <your.email@example.com>
2025-12-17 16:38:16 +0530

    State and prop assignment

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 import './App.css'
 import FirstComponent from './FirstComponent'
 import MainPage from './MainPage'
+import Counter from './counter.jsx'
+import Calculator from './Calculator.jsx'
+import MessageCard from './MessageCard.jsx'
+import UserProfile from './UserProfile.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +14,30 @@ function App() {
     <>
       <FirstComponent />
       <MainPage />
+
+      <h2>Counter App</h2>
+      <Counter />
+
+      <h2>Calculator App</h2>
+      <Calculator />
+
+      <h2>Message Cards</h2>
+
+      <MessageCard
+        title="Welcome"
+        message="Welcome to learning React components and props."
+      />
+
+      <MessageCard
+        title="Reminder"
+        message="Reuse components to keep your code clean."
+      />
+
+      <MessageCard
+        title="Tip"
+        message="Props help pass data from parent to child components."
+      />
+      <UserProfile />
     </>
   )
 }
